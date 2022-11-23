437a9bd45dedd6a83f0d130d83b99d8b60dca7ec
pzagmel <pedro.zagalm@gmail.com>
2022-11-23 00:05:22 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [tareas, nueva] = useState([
+    "Make the bed",
+    "Eat",
+    "Walk the dog",
+  ]);
+  const [input, setInput] = useState("");
+  return (
+    <div className="container">
+      <h1> Todo List</h1>
+      <ul>
+        <li>
+      <input
+        placeholder="What needs to be done"
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            nueva([...tareas, e.target.value]);
+            e.target.value = "";
+          }
+        }}
+      />
+</li>
+      {tareas.map((value, index) => {
+        return (
+          <li key={index}>
+            {value}
+            <i
+              className="fas fa-times float-end my-1 mx-1"
+              onClick={() =>
+                nueva(tareas.filter((target, Index) => index != Index))
+              }
+            ></i>
+          </li>
+        );
+    })}
+    <div className ="left mx"> {tareas.length} Item left </div>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
